9ac4c04b30beb4e8cfca971fb6e4bc604e27df35
Phil Pursglove <philpursglove@users.noreply.github.com>
2017-10-08 21:15:40 +0100

    Remove unused usings

--- MyTaskListApp/Controllers/HomeController.cs
+++ MyTaskListApp/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using MyTaskListApp.Models;
-using System.Configuration;
 
 namespace MyTaskListApp.Controllers
 {
